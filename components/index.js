0504f51ea417c0a267119b1b3bbfb714d71857a3
JP <jbalutr@gmail.com>
2022-02-22 12:22:08 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { PostCard } from './PostCard';
+export { Categories } from './Categories';
+export { PostWidget } from './PostWidget';
+export { Header } from './Header';
+export { Layout } from './Layout';
